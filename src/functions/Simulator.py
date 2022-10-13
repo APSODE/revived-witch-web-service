cb5c16ef6bde6fb181cd2c134174659b6a6b553c
APSODE <2gunbo74@gmail.com>
2022-10-13 15:22:08 +0900

    mongoDB에서 데이터를 가져와서 json format string를 dict로 변환하는 기능 추가

--- src/functions/Simulator.py
+++ src/functions/Simulator.py
@@ -1,37 +1,30 @@
-
+from database.DataBaseController import DataBaseController
+import json
+from bson import json_util
 
 class Simulator:
-    def __init__(self):
-        self._SIMULATE_DATA = {
-            "GACHA": {
-                "DOLL": {
-                    "PICKUP": {
-                        "ACTIVE": True,
-                        "TARGET": "안젤린"
-                    },
-
-                    "PROBABILITY": {
-                        "UR": 2,
-                        "SSR": 8,
-                        "SR": 40,
-                        "R": 50
-                    },
-
-                    "TYPE": "DREAM",
-
-                    "DOLL_DATA": {
-                        "UR": [],
-                        "SSR": [],
-                        "SR": [],
-                        "R": []
-                    }
-
-                }
-            }
-        }
-
-    def _SetInitialData(self) -> None:
-        pass
+    def __init__(self, banner_name: str):
+        self.database_controller = DataBaseController("Development_Database")
+        self._SIMULATE_BANNER_DATA = self.database_controller.FindDatas(
+            collection_name = "GachaBanner",
+            query = {"banner_name": banner_name},
+            find_one = True
+        )
 
     def _SelectGrade(self):
         pass
+
+    def Test(self):
+        with open("TEST_DATA.json", "w") as WRITE_PROFILE:
+            result = self._SIMULATE_BANNER_DATA
+            json_data = json.dumps(result, default = json_util.default)
+            json.dump(json.loads(json_data), WRITE_PROFILE, indent = 4)
+
+    def SimulateGacha(self):
+        pass
+
+
+if __name__ == "__main__":
+
+    gacha_simulator = Simulator(banner_name = "영혼 소환")
+    gacha_simulator.Test()
