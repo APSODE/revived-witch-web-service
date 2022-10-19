c89f4181a58a876e847a994fb5f54b7572b91236
APSODE <2gunbo74@gmail.com>
2022-10-20 08:34:51 +0900

    테스트 템플릿 뷰

---
+++ src/views/TestView.py
@@ -0,0 +1,28 @@
+from flask import Blueprint, redirect, render_template, url_for, request, jsonify
+
+
+
+BP = Blueprint("Test", __name__,
+               url_prefix = "/test",
+               template_folder = "../templates",
+               static_folder = "../static",
+               )
+
+@BP.route("/dashboard")
+def TestDashboard():
+    return render_template("TestTemplates/Dashboard_Test.html")
+
+@BP.route("/cover")
+def TestCover():
+    return render_template("TestTemplates/Cover_Test.html")
+
+@BP.route("/ajax")
+def TestAjax():
+    return render_template("TestTemplates/Ajax_Test.html")
+
+@BP.route('/ajax', methods=['POST'])
+def Ajax():
+    data = request.get_json()
+    print(data)
+
+    return jsonify(result = "success", result2= data)
