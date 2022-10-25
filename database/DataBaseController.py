69736908d280fe68d2ecfddd2f8c7a84a84123c4
APSODE <2gunbo74@gmail.com>
2022-10-25 16:38:52 +0900

    DB계정 환경변수에서 접근하도록 변경

--- database/DataBaseController.py
+++ database/DataBaseController.py
@@ -30,8 +30,11 @@ class READ_WRITE:
 class DataBaseController:
     def __init__(self, db_name: str):
         self._account_file_dir = os.path.dirname(os.path.abspath(__file__)) + "\\mongoDB_account.json"
-        self._account = READ_WRITE.READ_JSON(FILE_DIR = self._account_file_dir).get('account')
-
+        # self._account = READ_WRITE.READ_JSON(FILE_DIR = self._account_file_dir).get('account')
+        self._account = {
+            "id": os.environ.get("id"),
+            "pw": os.environ.get("pw")
+        }
         self._client = MongoClient(f"mongodb+srv://{self._account.get('id')}:{self._account.get('pw')}"
                                    f"@rw-webservice-database.bbsoap3.mongodb.net/?retryWrites=true&w=majority")
 
