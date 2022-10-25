5ec896641c5ad2e64985596fa9a608e94c2edb4b
APSODE <2gunbo74@gmail.com>
2022-10-25 16:10:05 +0900

    import최적화

--- database/models/gacha_banner_model.py
+++ database/models/gacha_banner_model.py
@@ -1,11 +1,7 @@
-
-
-
-
 class BannerData:
     def __init__(self, banner_name: str, pick_up_data: dict = None, probability: dict = None):
         self._banner_name = banner_name
-        
+
         if pick_up_data is not None:
             self._pick_up_data = pick_up_data
         else:
@@ -13,10 +9,10 @@ class BannerData:
                 "active": False,
                 "target": ""
             }
-        
+
         if probability is not None:
             self._probability = probability
-            
+
         else:
             self._probability = {
                 "UR": 2,
@@ -24,20 +20,18 @@ class BannerData:
                 "SR": 40,
                 "R": 50
             }
-            
-            
+
     @property
     def BannerName(self) -> str:
         return self._banner_name
-    
+
     @property
     def PickUpData(self) -> dict:
         return self._pick_up_data
-    
+
     @property
     def Probability(self) -> dict:
         return self._probability
-
 
     def GetAllBannerData(self) -> dict:
         RT_DICT = {
@@ -52,22 +46,17 @@ class BannerData:
         RT_STR = f"\n=================================\n" \
                  f"BANNER_NAME : {self._banner_name}\n" \
                  f"PICK_UP_DATA: {self._pick_up_data}\n" \
-                 f"PROBABILITY: {self._probability}\n" \ 
+                 f"PROBABILITY: {self._probability}\n" \
                  f"\n=================================\n"
 
         return RT_STR
-        
 
 
 class Banner:
     @staticmethod
-    def SerializeData(target_data: dict) -> DollData:
+    def SerializeData(target_data: dict) -> BannerData:
         return BannerData(
-            banner_name = target_data.get("BANNER_NAME"),
-            pick_up_data = target_data.get("PICK_UP_DATA"),
-            probability = target_data.get("PROBABILITY")
+            banner_name=target_data.get("BANNER_NAME"),
+            pick_up_data=target_data.get("PICK_UP_DATA"),
+            probability=target_data.get("PROBABILITY")
         )
-
-        
-    
-    
