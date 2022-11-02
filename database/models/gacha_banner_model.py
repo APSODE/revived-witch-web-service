8f7ed76b8c1095315a589b795a5fc3464f57ae29
APSODE <2gunbo74@gmail.com>
2022-11-02 11:31:04 +0900

    픽업 배너 Data Transfer Object 변경

--- database/models/gacha_banner_model.py
+++ database/models/gacha_banner_model.py
@@ -1,6 +1,26 @@
+from typing import List, Dict
+
 class BannerData:
-    def __init__(self, banner_name: str, pick_up_data: dict = None, probability: dict = None):
+    def __init__(
+            self,
+            banner_name: str,
+            banner_type: str,
+            stack_info: dict = None,
+            pick_up_data: dict = None,
+            probability: dict = None,
+            summonable_dolls: dict = None
+
+    ):
         self._banner_name = banner_name
+        self._banner_type = banner_type
+
+        if stack_info is not None:
+            self._stack_info = stack_info
+        else:
+            self._stack_info = {
+                "half": 80,
+                "full": 160
+            }
 
         if pick_up_data is not None:
             self._pick_up_data = pick_up_data
@@ -21,23 +41,67 @@ class BannerData:
                 "R": 50
             }
 
+        if summonable_dolls is not None:
+            self._summonable_dolls = summonable_dolls
+        else:
+            witch_data = {
+                "NAME": "마녀",
+                "GRADE": "SR",
+                "ELEMENT": "Aether",
+                "DOLL_CLASS": "Witch",
+                "LIMITED": False
+            }
+            self._summonable_dolls = {
+                "UR": [witch_data],
+                "SSR": [witch_data],
+                "SR": [witch_data],
+                "R": [witch_data]
+            }
+
+
+
+
+
+
+
     @property
     def BannerName(self) -> str:
         return self._banner_name
 
     @property
+    def BannerType(self) -> str:
+        return self._banner_type
+
+    @property
+    def StackInfo(self) -> dict:
+        return self._stack_info
+
+    @property
     def PickUpData(self) -> dict:
+        """
+        return dict는 2가지의 key-value를 가진다.\n
+        first key-value | active - <bool>\n
+        second key-value | pick_up_doll_name - <str>\n
+        :return:
+        """
         return self._pick_up_data
 
     @property
     def Probability(self) -> dict:
         return self._probability
 
+    @property
+    def SummonableDolls(self) -> Dict[str, List[dict]]:
+        return self._summonable_dolls
+
     def GetAllBannerData(self) -> dict:
         RT_DICT = {
             "BANNER_NAME": self._banner_name,
+            "BANNER_TYPE": self._banner_type,
+            "STACK_INFO": self._stack_info,
             "PICK_UP_DATA": self._pick_up_data,
-            "PROBABILITY": self._probability
+            "PROBABILITY": self._probability,
+            "SUMMONABLE_DOLLS": self._summonable_dolls
         }
 
         return RT_DICT
@@ -45,8 +109,11 @@ class BannerData:
     def __repr__(self):
         RT_STR = f"\n=================================\n" \
                  f"BANNER_NAME : {self._banner_name}\n" \
+                 f"BANNER_TYPE : {self._banner_type}\n" \
+                 f"STACK_INFO : {self._stack_info}\n" \
                  f"PICK_UP_DATA: {self._pick_up_data}\n" \
                  f"PROBABILITY: {self._probability}\n" \
+                 f"SUMMONABLE_DOLLS : {self._summonable_dolls}\n" \
                  f"\n=================================\n"
 
         return RT_STR
@@ -57,6 +124,9 @@ class Banner:
     def SerializeData(target_data: dict) -> BannerData:
         return BannerData(
             banner_name=target_data.get("BANNER_NAME"),
+            banner_type=target_data.get("BANNER_TYPE"),
+            stack_info=target_data.get("STACK_INFO"),
             pick_up_data=target_data.get("PICK_UP_DATA"),
-            probability=target_data.get("PROBABILITY")
+            probability=target_data.get("PROBABILITY"),
+            summonable_dolls=target_data.get("SUMMONABLE_DOLLS")
         )
